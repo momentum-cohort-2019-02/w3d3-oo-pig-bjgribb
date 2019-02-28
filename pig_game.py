16ab15a0d51d55a8994a22200157878168260c2b
bjgribb <bjgribb@gmail.com>
2019-02-27 21:21:30 -0500

    rough classes and added tests

--- pig_game.py
+++ pig_game.py
@@ -1,53 +1,53 @@
 import random
 
-class Scoresheet:
-    """Roll die and return value, option for player to hold or roll again, ends when 1 is rolled, otherwise compiles rolls for turn score."""
+def roll_dice():
+    return random.randint(1, 6)
 
+class ComputerPlayer:
+    """
+    Computer oppoenent in the game of Pig. Rolls die until either 20 points or a 1 is rolled.
+    """
     def roll_die(self):
-        """Randomly picks a number between 1 and 6 and lets user know value."""
-        die_value = random.randint(1, 6)
-        if die_value != 1:
-            print(f"You've rolled a {die_value}.")
-            return int(die_value)
-        else:
-            print(f"Uh-oh, you rolled a 1, no points for you.")
-            return 0
+        computerscore = 0
+        while computerscore < 20:
+            roll = roll_dice()
+            if roll != 1:
+                computerscore += roll
+                print(f"The computer rolled a {roll}")
+            else:
+                print(f"The computer rolled a 1.")
+                computerscore = 0
+                break
+        print(f"\nComputer turn is over, it scored {computerscore} for this round.")
+        return computerscore
 
-    def roll_again(self):
-        """allows player to select if they would like to roll again or hold"""
-        roll_option = input("Would you like to roll again (y/n): ")
-        if roll_option.casefold().strip() == "y":
-            return True
-        else:
-            return False
-
-    def turn_score(self):
-        """As long as the player is choosing to roll again and doesn't roll a 1, compile the score"""
-        # turn.roll_die()
-        # turn.roll_again()
-        turn_score = 0
-        while turn.roll_again() == True:
-            single_roll_score = turn.roll_die()
-            single_roll_score += turn_score
-            return turn_score
-
-
-class Player:
-    """This is the human player, responsiblity to compile turn scores from scoresheet"""
-
-class Computer:
-    """This is the computer player, responsiblity to compile turn scores from scoresheet"""
-
-class Pig:
-    """This is the main game and will initiate and run thru the game of Pig. Calls out winner when player reaches 100+ points."""
-    pass
+class HumanPlayer:
+    """
+    Human player who choses to either roll or hold
+    """
+    def roll_die(self):
+        playerscore = 0
+        roll_hold_option = "y"
+        while roll_hold_option.strip().casefold() == "y":
+            roll = roll_dice()
+            if roll != 1:
+                playerscore += roll
+                print(f"\nYou rolled a {roll}, your current score for this round is {playerscore}")
+                roll_hold_option = input("Would you like to roll again (y/n): ")
+            elif roll == 1:
+                print(f"\nUh-oh you rolled a 1!")
+                playerscore = 0
+                break
+        print(f"\nYour turn is over, your score for this round is {playerscore}.")
+        return playerscore
 
 if __name__ == "__main__":
     
-    turn = Scoresheet()
+    computer = ComputerPlayer()
+    player = HumanPlayer()
 
-    turn.roll_die()
-
+    print(computer.roll_die())
+    print(player.roll_die())
 
 
 
--- test_pig_game.py
+++ test_pig_game.py
@@ -1,8 +1,15 @@
-from pig_game_basic import pig_winner
+from pig_game import roll_dice, ComputerPlayer
 
-#def test_pig_winner():
-assert pig_winner(106, 98) == "You're the winner!! You scored 106 vs the computer's 98!"
-assert pig_winner(87, 108) == "You lose. Computer: 108 You: 87"
+def test_roll_dice():
+    assert roll_dice() in [1, 2, 3, 4, 5, 6]
+
+def test_comp_roll_die():
+    computer = ComputerPlayer()
+    computerscore = computer.roll_die()
+    assert computerscore == 0 or computerscore >= 20
+    assert computerscore not in range(1,21)
+
+
 
 
 
