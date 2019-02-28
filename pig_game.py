51c1d52b54ad9ebf723f8e97806144328dd7a315
bjgribb <bjgribb@gmail.com>
2019-02-27 23:47:48 -0500

    farther along

--- pig_game.py
+++ pig_game.py
@@ -1,11 +1,17 @@
 import random
 
+# Need option to play again
+# Not much error handling for incorrect inputs throughout (i.e. roll again for HumanPlayer)
+# Game could randomize assignment of starting player
+# Work on logic of breaking while loop when player wins (still let's computer roll)
+
+
 def roll_dice():
     return random.randint(1, 6)
 
 class ComputerPlayer:
     """
-    Computer oppoenent in the game of Pig. Rolls die until either 20 points or a 1 is rolled.
+    Computer oppoenent in the game of Pig. Rolls die until either 20 points or a 1 is rolled.  Compiles score for current turn.
     """
     def roll_die(self):
         computerscore = 0
@@ -21,9 +27,13 @@ class ComputerPlayer:
         print(f"\nComputer turn is over, it scored {computerscore} for this round.")
         return computerscore
 
+    def announce_win(self, computer_score):
+        if computer_score >= 100:
+            print(f"You lose.")
+
 class HumanPlayer:
     """
-    Human player who choses to either roll or hold
+    Human player who choses to either roll or hold and this compiles their score for current turn.
     """
     def roll_die(self):
         playerscore = 0
@@ -41,14 +51,54 @@ class HumanPlayer:
         print(f"\nYour turn is over, your score for this round is {playerscore}.")
         return playerscore
 
+    def announce_win(self, player_score):
+        if player_score >= 100:
+            print(f"You're the winner!!")
+
+        
+
+class PigGame:
+    """Runs through the game with the two participants, computer and human player"""
+    def __init__(self, player, computer):
+        self.player = player
+        self.computer = computer
+
+    def pig_summary(self, player_score, computer_score):
+        print(f"""Thanks for playing! Still working on the play again feature...
+        \nComputer: {computer_score} You: {player_score}""")
+
+
+    def run_pig(self):
+        player_score = 0
+        computer_score = 0
+
+        print(f"Welcome to the game of Pig. It's you versus the computer.")
+
+        while player_score < 100 and computer_score < 100: 
+        
+            if not self.player.announce_win(player_score):
+                player_score += HumanPlayer.roll_die(self)
+                print(f"Your total score is {player_score}.\n")
+            if not self.computer.announce_win(computer_score):
+                computer_score += ComputerPlayer.roll_die(self)
+                print(f"The computer's total score is {computer_score}.\n")
+        if player_score > computer_score:
+            self.player.announce_win(player_score)
+        else:
+            self.computer.announce_win(computer_score)
+
+        self.pig_summary(player_score, computer_score)
+
+
+
 if __name__ == "__main__":
     
     computer = ComputerPlayer()
     player = HumanPlayer()
 
-    print(computer.roll_die())
-    print(player.roll_die())
+    pig_game = PigGame(player, computer)
 
+    pig_game.run_pig()
 
 
  
